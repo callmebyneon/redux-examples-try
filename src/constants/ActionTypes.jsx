e5403fe6c0d0041caa6a8a57ed3546c19441aafc
Na-yeon YI <2n.ayeon.yi@gmail.com>
2022-04-13 10:35:36 +0900

    Update ActionTypes.jsx

--- src/constants/ActionTypes.jsx
+++ src/constants/ActionTypes.jsx
@@ -5,9 +5,3 @@ export const COMPLETE_TODO = 'todos/COMPLETE_TODO';
 export const COMPLETE_ALL_TODOS = 'todos/COMPLETE_ALL_TODOS';
 export const CLEAR_COMPLETED = 'todos/CLEAR_COMPLETED';
 export const SET_VISIBILITY_FILTER = 'todos/SET_VISIBILITY_FILTER';
-
-export const ADD_TO_CART = 'cart/ADD_TO_CART';
-export const CHECKOUT_REQUEST = 'cart/CHECKOUT_REQUEST';
-export const CHECKOUT_SUCCESS = 'cart/CHECKOUT_FAILURE';
-export const CHECKOUT_FAILURE = 'cart/CHECKOUT_FAILURE';
-export const RECEIVE_PRODUCTS = 'cart/RECEIVE_PRODUCTS';
