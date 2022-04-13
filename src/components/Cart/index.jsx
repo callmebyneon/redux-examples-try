d7eda5a3ca2ade2a03762f6cbac4538763252b14
Na-yeon YI <2n.ayeon.yi@gmail.com>
2022-04-13 10:36:54 +0900

    Update index.jsx

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,42 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import Product from './Product';
-
-const Cart = ({ products, total, onCheckoutClicked }) => {
-  const hasProducts = Boolean(products?.length > 0);
-  const nodes = hasProducts ? (
-    products.map(prod =>
-      <Product
-        title={prod.title}
-        price={prod.price}
-        quantity={prod.quantity}
-        key={prod.id}
-      />
-    )
-  ) : (
-    <em>Please add some porducts to cart.</em>
-  )
-  
-  return (
-    <div>
-      <h3>Your Cart</h3>
-      <div>{nodes}</div>
-      <p>Total: &#36;{total}</p>
-      <button
-        onClick={onCheckoutClicked}
-        disabled={hasProducts ? '' : 'disabled'}
-      >
-        Checkout
-      </button>
-    </div>
-  );
-};
-
-Cart.propTypes = {
-  products: PropTypes.array,
-  total: PropTypes.string,
-  onCheckoutClicked: PropTypes.func
-}
+const Cart = () => (
+  <h1>Cart</h1>  
+);
 
 export default Cart;
